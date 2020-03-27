530943ccabb8bd3f6f7edee5057413464fad35bd
Mandy Arens <40013860+maanderz@users.noreply.github.com>
2020-03-27 13:30:05 -0600

    modals
    got modal to show when btn clicked

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 import Nav from "./Components/Nav";
 
 export default function App() {
+  const [notes, setNote] = useState([]);
+
+  const addNote = () => {
+    setNote([
+      ...notes,
+      {
+        id: notes.length,
+        value: Math.random()
+      }
+    ]);
+  };
+
   return (
     <div className="App">
-      <Nav />
+      <Nav addNote={addNote} />
+      <ul>
+        {notes.map(item => (
+          <li key={item.id}>{item.value}</li>
+        ))}
+      </ul>
     </div>
   );
 }
--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,13 +1,26 @@
 import React from "react";
 import "./Nav.css";
+import NewNote from "./NewNote";
 
 function Nav() {
+  const [modalShow, setModalShow] = React.useState(false);
+
+  // const openNewNote = () => {
+  //   console.log("here");
+  //   return <NewNote />;
+  // };
+
   return (
     <div className="title">
       <h1> Your Notes: </h1>
-      <button type="button" class="btn btn-warning">
+      <button
+        type="button"
+        className="btn btn-warning"
+        onClick={() => setModalShow(true)}
+      >
         New Note
       </button>
+      <NewNote show={modalShow} onHide={() => setModalShow(false)} />
     </div>
   );
 }
